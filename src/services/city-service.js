dfc72ffc1dce916417186fe30ca2c02ca5d06c26
Suraj <kunwarsuraj19@gmail.com>
2023-11-17 19:14:21 +0530

    city service addded

---
+++ src/services/city-service.js
@@ -0,0 +1,46 @@
+const {CityRepository}   = require("../repository/index")
+
+class CityService {
+    constructor(){
+        this.cityRepository = new CityRepository() 
+    }
+
+
+    async createCity(data){
+        try{
+           const  city = await this.cityRepository.createCity(data)
+            return city
+        }catch(err){
+            console.log("Something wrong in service layer")
+            throw {err}
+        }
+        
+    }
+    async deleteCity(cityId){
+        try{
+            const response = await this.cityRepository.deleteCity(cityId)
+            return response
+        }catch(err){
+            console.log("Something wrong in service layer")
+            throw {err}
+        }
+    }
+    async getCity(cityId){
+        try{
+            const  city = await this.cityRepository.getCity(cityId)
+            return city
+        }catch(err){
+            console.log("Something wrong in service layer")
+            throw {err}
+        }
+    }
+    async updateCity(cityId  , data){
+        try{
+            const city = await this.cityRepository.updateCity(data , cityId)
+            return city;
+        }catch(err){
+            console.log("Something wrong in service layer")
+            throw {err}
+        }
+    }
+}
