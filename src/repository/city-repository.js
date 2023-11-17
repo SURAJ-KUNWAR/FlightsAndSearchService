83f91019c5ff1e9e1b1c3e8a233871679b7f0097
Suraj <kunwarsuraj19@gmail.com>
2023-11-17 16:17:36 +0530

    city repo completed

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -6,10 +6,8 @@ class CityRepository {
             const city = await City.create({ name });
             return city;
         }catch(error){
-            console.log(error)
-            throw {
-               
-                error}
+            console.log("Something wnet wrong in city repository")
+            throw{error}
         }
     }
 
@@ -21,9 +19,37 @@ class CityRepository {
             }
          })
         }catch(error){
+            console.log("Something wnet wrong in city repository")
             throw {error}
         }
     }
+
+    async getCity(cityId){
+        try {
+            const city  = City.findByPk(cityId)
+            return city 
+        }catch(err){
+            console.log("Something wnet wrong in city repository")
+            throw{err}
+        }
+    }
+
+
+    async updateCity (data , cityId){
+        try{
+            const city = City.update (data , {where : {
+                id : cityId
+            }})
+            return city
+        }catch(err){
+            console.log("Something wnet wrong in city repository")
+            throw{err}
+        }
+        
+    }
+        
+      
+    
 }
 
 module.exports = CityRepository;
